7fd99d01f00c5201b80b383556a25d807075d1ce
Malarka <haerfaer2@gmail.com>
2025-08-20 20:23:37 +0500

    added 2 more buttons before using useReducer

--- second-react-app/src/App.jsx
+++ second-react-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => setSeconds(s => s + 1), 1000);
 
-    return () => clearInterval(interval); // cleanup
+    return () => clearInterval(interval);
   }, []);
 
   function Child() {
@@ -25,12 +25,15 @@ function App() {
       <div>
        <h1>Count: {count}</h1>
        <button onClick={() => setCount(count + 1)}>Increase</button>
+       <button onClick={() => setCount(count - 1)}>Decrease</button>
+       <button onClick={() => setCount(0)}>Reset</button>
       </div>
       <h1>
         Timer: {seconds}
       </h1>
       <input ref={inputRef} placeholder="Type here..." />
       <button onClick={() => inputRef.current.focus()}>Focus Input</button>
+
       <ThemeContext.Provider value="dark">
       <Child />
      </ThemeContext.Provider>
