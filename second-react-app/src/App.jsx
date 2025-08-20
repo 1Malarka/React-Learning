84376ea3622c7d4b9e03f1aff93b8d18bbd7a477
Malarka <haerfaer2@gmail.com>
2025-08-20 20:49:31 +0500

    app.jsx for some reason wasn't attached.

--- second-react-app/src/App.jsx
+++ second-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect, useRef, createContext, useContext } from 'react'
+import { useState, useEffect, useRef, createContext, useContext, useReducer } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const inputRef = useRef();
   const ThemeContext = createContext("light");
+  const initialState = { count: 0 };
+  const [state, dispatch] = useReducer (reducer, initialState)
+
   
 
   useEffect(() => {
@@ -18,15 +21,31 @@ function App() {
     return <h1>Theme: {theme}</h1>;
   }   
 
-  
+  function reducer(state, action) {
+    switch(action.type){
+      case "INCREMENT" : 
+        console.log("increased number by 1")
+        return { count: state.count + 1}
+      case "DECREMENT" :
+        console.log("decreased number by 1")
+        return { count: state.count - 1}
+      case "RESET" :
+        console.log("reseted")
+        return { count: 0}
+      default:
+        return state;
+    }
+  }
+
+
 
   return (
     <>
       <div>
-       <h1>Count: {count}</h1>
-       <button onClick={() => setCount(count + 1)}>Increase</button>
-       <button onClick={() => setCount(count - 1)}>Decrease</button>
-       <button onClick={() => setCount(0)}>Reset</button>
+       <h1>Count: {state.count}</h1>
+       <button onClick={() => dispatch({type: "INCREMENT"})}>Increase</button>
+       <button onClick={() => dispatch({type: "DECREMENT"})}>Decrease</button>
+       <button onClick={() => dispatch({type: "RESET"})}>Reset</button>
       </div>
       <h1>
         Timer: {seconds}
