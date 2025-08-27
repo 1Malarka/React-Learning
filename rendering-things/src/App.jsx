ec8a68ef1ef062ab0c5345fddfc67d782a733c72
Malarka <haerfaer2@gmail.com>
2025-08-27 14:49:28 +0500

    Forgot to commit new feature, Refs.

--- rendering-things/src/App.jsx
+++ rendering-things/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef} from "react";
 
 function Rendering() {
   const [count, setCount] = useState(0);
@@ -8,6 +8,7 @@ function Rendering() {
     { id: 3, name: "Charlie" }
   ];
   const [pos, setPos] = useState({ x: 0, y: 0 });
+  const divRef = useRef(null);
 
 
   useEffect(() => {
@@ -25,6 +26,11 @@ function Rendering() {
     setPos({ x: e.clientX, y: e.clientY });
  }
 
+ const handleClick = () => {
+  divRef.current.textContent = "Hello from useRef!";
+  divRef.current.focus();
+ }
+
   return ( 
   <>
     <h1>Time: {count}</h1>; 
@@ -36,6 +42,12 @@ function Rendering() {
     <div style={{ height: "200px", border: "1px solid black" }}
          onMouseMove={handleMouseMove}>
       <p>Mouse position: {pos.x}, {pos.y}</p>
+    </div>
+    <div>
+      <div ref={divRef}>
+       Something in div...
+      </div>
+       <button onClick={handleClick}>Change and focus</button>
     </div>
  </>
   )
