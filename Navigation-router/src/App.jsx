79faef7216f6c653d425a2255124257e6bdc948e
Malarka <haerfaer2@gmail.com>
2025-09-04 10:59:34 +0500

    Added styling to project, bcs it looked awful

--- Navigation-router/src/App.jsx
+++ Navigation-router/src/App.jsx
@@ -8,13 +8,15 @@ function Layout() {
   return (
     <div>
       <header>
-        <h1>Super App</h1>
+        <h2>Super App</h2>
+        <div className="Links"> 
         <nav>
           <Link to="/">Home</Link> |{" "}
           <Link to="/App0">App1</Link> |{" "}
           <Link to="/App1">App2</Link> |{" "}
           <Link to="/App2">App3</Link>
         </nav>
+        </div>
       </header>
 
       <main>
@@ -31,7 +33,7 @@ const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: "/", element: <h2>Welcome!</h2> },
+      { path: "/", element: <h3>Welcome!</h3> },
       { path: "/App0", element: <App0 /> },
       { path: "/App1", element: <App1 /> },
       { path: "/App2", element: <App2 /> },
